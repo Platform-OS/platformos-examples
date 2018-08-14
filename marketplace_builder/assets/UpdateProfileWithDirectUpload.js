afa51837a4b63e950705a52565b9a6a801425f4c
Paweł Kowalski <pavelloz@gmail.com>
2018-08-15 00:14:56 +0200

    Finish update profile with image in the background

--- marketplace_builder/assets/UpdateProfileWithDirectUpload.js
+++ marketplace_builder/assets/UpdateProfileWithDirectUpload.js
@@ -22,9 +22,9 @@
     const previewHtml = `
       <figure class="figure mr-3 w-25 mw-25">
         <a href="${imageUrl}" target="_blank">
-          <img src="${imageUrl}" width="200" class="figure-img img-fluid rounded">
+          <img src="${imageUrl}" width="150" class="figure-img img-fluid rounded">
         </a>
-        <figcaption class="figure-caption">${getFileName()}</figcaption>
+        <figcaption class="figure-caption">New image: ${getFileName()}</figcaption>
       </figure>
     `;
 
@@ -42,7 +42,6 @@
     const formdata = new FormData();
 
     $presignFields.forEach(field => {
-      // console.log('Adding to formdata: ', `${field.name} : ${field.value}`);
       formdata.append(field.name, field.value);
     });
 
