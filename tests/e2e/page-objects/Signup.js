06d3a0258cbe29c0ff751af1d2f29c5d675d5232
Jacek Norbert <jaceknorbert@iMac-Jacek.local>
2018-12-07 09:55:15 +0100

    Small fixes in code

--- tests/e2e/page-objects/Signup.js
+++ tests/e2e/page-objects/Signup.js
@@ -3,11 +3,10 @@ import { Selector } from 'testcafe';
 export default class SignUp {
     constructor() {
         this.headerSignUp = Selector('a').withText('Sign up');
-        this.headerSignUp = Selector('a').withText('Log in');
+        this.headerLogIn = Selector('a').withText('Log in');
         this.firstname = Selector('#form_first_name');
         this.email = Selector('#form_email');
         this.developerSignUp = Selector('a').withText('I am developer');
-        this.firstname = Selector('#form_first_name');
         this.email = Selector('#form_email');
         this.password = Selector('#form_password');
         this.mobileNumber = Selector('#form_mobile_number');
