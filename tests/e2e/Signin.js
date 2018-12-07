8b58d94c4f157f61491a33d9815c5cdf636812d4
Jacek Norbert <jaceknorbert@iMac-Jacek.local>
2018-12-07 13:20:32 +0100

    Sing In

---
+++ tests/e2e/Signin.js
@@ -0,0 +1,20 @@
+import 'testcafe';
+import LayoutPage from './page-objects/Layout';
+import HomePage from './page-objects/Homepage';
+import SignIn from './page-objects/Signin';
+
+const signIn = new SignIn();
+const homePage = new HomePage();
+const layoutPage = new LayoutPage();
+
+fixture('Homepage').page(layoutPage.URL.staging);
+
+test('Log in to the Dashboard', async t => {
+
+  await t 
+  .click(homePage.linkLogIn)
+
+  await signIn.signin('test_user@test.com','password')
+  .expect(layoutPage.alertSuccess.exists).ok();
+});
+  
---
+++ tests/e2e/page-objects/Signin.js
@@ -0,0 +1,14 @@
+import 'testcafe';
+import SignUp from './Signup';
+
+const signUp = new SignUp();
+
+export default class SignIn { 
+
+    async signin (username,password) {
+        await t
+            .typeText(signUp.email, username)
+            .typeText(signUp.password, password)
+            .click(signUp.submitButton);
+    }
+}
