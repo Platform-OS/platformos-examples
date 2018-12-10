3323862140331ae52bab1774d0ebdf9cb1ed8880
Jacek Norbert <jaceknorbert@iMac-Jacek.local>
2018-12-10 10:27:13 +0100

    Additional steps for login scenarios

--- tests/e2e/Login.js
+++ tests/e2e/Login.js
@@ -9,6 +9,14 @@ const layoutPage = new LayoutPage();
 
 fixture('Homepage').page(layoutPage.URL.staging);
 
+test('Display error message on invalid password', async t => {
+  const error = 'Invalid email or password';
+
+  await t.click(homePage.linkLogIn);
+  await logIn.signin('test@placek.com', 'wrongpassword');
+  await t.expect(logIn.errorText.withText(error).exists).ok();
+});
+
 test('Log in to the Dashboard', async t => {
   await t.click(homePage.linkLogIn);
   await logIn.signin('test_user@test.com', 'password');
--- tests/e2e/page-objects/Login.js
+++ tests/e2e/page-objects/Login.js
@@ -1,9 +1,12 @@
-import { t } from 'testcafe';
+import { Selector, t } from 'testcafe';
 import Register from './Register';
 
 const register = new Register();
 
 export default class LogIn {
+  constructor() {
+    this.errorText = Selector('.form-group p');
+  }
   async signin(username, password) {
     await t
       .typeText(register.email, username)
