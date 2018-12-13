8501cce7eb2d2b6626c16df9fceb181ebaa8990a
Jacek <jacek@MacBook-Pro-Jacek.local>
2018-12-13 15:22:08 +0100

    Uploading Files Directly to Amazon S3

--- tests/e2e/UpdateProfile.js
+++ tests/e2e/UpdateProfile.js
@@ -10,8 +10,21 @@ const layoutPage = new LayoutPage();
 const notifications = new Notifications();
 const updateProfile = new UpdateProfile();
 const homePage = new HomePage();
+const uploadedPreviewImages = layoutPage.Content.find(
+  updateProfile.files.imgAWS
+);
 
 fixture('Update profile').page(layoutPage.URL.staging);
+
+test('There are no liquid errors on the page', async t => {
+  await logIn.login('test_user@test.com', 'password');
+  await t
+    .expect(notifications.messageType.success.innerText)
+    .eql(notifications.text.login);
+  await t.click(homePage.link.uploadFiles);
+  await layoutPage.checkLiquidErrors();
+  await t.expect(updateProfile.link.documentation.exists).ok();
+});
 
 test('Direct upload using AJAX', async t => {
   await logIn.login('test_user@test.com', 'password');
@@ -22,8 +35,51 @@ test('Direct upload using AJAX', async t => {
     .click(homePage.link.uploadFiles)
     .setFilesToUpload(updateProfile.input.ajaxUpload, ['./uploads/hero.png']);
   await t
-    .expect(updateProfile.files.img.exists)
-    .ok()
     .expect(updateProfile.files.name.innerText)
-    .eql('hero.png');
+    .eql('hero.png')
+    .expect(uploadedPreviewImages.count)
+    .eql(1);
+});
+
+test('Uploading Files Directly to Amazon S3 and using uploaded file as an avatar', async t => {
+  const previewImages = layoutPage.Content.find(updateProfile.files.img);
+
+  await logIn.login('test_user@test.com', 'password');
+  await t
+    .expect(notifications.messageType.success.innerText)
+    .eql(notifications.text.login)
+    .click(homePage.link.uploadFiles);
+  await t
+    .expect(
+      updateProfile.files.currentImgName.withText(
+        updateProfile.txt.currentAvatar
+      ).exists
+    )
+    .ok()
+    .expect(
+      updateProfile.files.currentImgName.withText(
+        updateProfile.txt.currentBanner
+      ).exists
+    )
+    .ok();
+  await t.setFilesToUpload(updateProfile.input.avatarUpload, [
+    './uploads/bug.png',
+  ]);
+  await t.setFilesToUpload(updateProfile.input.banerUpload, [
+    './uploads/bug.png',
+  ]);
+  await t
+    .expect(
+      updateProfile.files.currentImgName.withText(updateProfile.txt.newAvatar)
+        .exists
+    )
+    .ok()
+    .expect(
+      updateProfile.files.currentImgName.withText(updateProfile.txt.newBanner)
+        .exists
+    )
+    .ok();
+
+  await t.expect(previewImages.count).eql(2);
+  await t.expect(uploadedPreviewImages.count).eql(2);
 });
--- tests/e2e/page-objects/UpdateProfile.js
+++ tests/e2e/page-objects/UpdateProfile.js
@@ -1,4 +1,4 @@
-import { Selector } from 'testcafe';
+import {Selector} from 'testcafe';
 
 export default class UpdateProfile {
   constructor() {
@@ -7,12 +7,27 @@ export default class UpdateProfile {
         process.env.MP_URL ||
         'https://nearme-example.staging-oregon.near-me.com/update_profile',
     };
+    this.txt = {
+      currentAvatar: 'Current avatar',
+      currentBanner: 'Current banner',
+      newAvatar: 'Newly uploaded avatar',
+      newBanner: 'Newly uploaded banner',
+    };
     this.input = {
       ajaxUpload: Selector('form[action]:nth-of-type(2) input[name="file"]'),
+      avatarUpload: Selector('form[action]:nth-of-type(3) input[name="file"]'),
+      banerUpload: Selector('input[name="file"]:nth-of-type(3)'),
     };
     this.files = {
       name: Selector('figcaption.figure-caption'),
-      img: Selector('img'),
+      currentImgName: Selector('p.text-muted'),
+      imgAWS: 'img[src*="amazonaws.com/uploads"]',
+      img: 'img[src*="uploads/image"]',
+    };
+    this.link = {
+      documentation: Selector('a').withText(
+        'Uploading files directly to Amazon S3 using AJAX'
+      ),
     };
   }
 }
