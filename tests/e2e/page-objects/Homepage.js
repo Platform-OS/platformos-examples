3b6b6f609b8eb2cb8ba24433b4a3cf6b73baef56
Paweł Kowalski <pavelloz@gmail.com>
2019-01-24 17:48:22 +0100

    Fix homepage test

--- tests/e2e/page-objects/Homepage.js
+++ tests/e2e/page-objects/Homepage.js
@@ -24,7 +24,19 @@ export default class HomePage {
       uploadFiles: Selector('a').withText('Uploading files directly to Amazon S3 using AJAX'),
       stripe: Selector('a').withText('Integrating Stripe payments'),
       ajax: Selector('a').withText('Managing customizations using AJAX (CRUD operations)'),
-      models: Selector('a').withText('Loading related models while avoiding n+1 queries. Increase speed 10x')
+      models: Selector('a').withText('Loading related models while avoiding n+1 queries. Increase speed 10x'),
+      simplePage: Selector('a').withText('Example of a very simple page'),
+      timeDiff: Selector('a').withText('Measuring execution time of liquid code fragments (time_diff)'),
+      module: Selector('a').withText('Creating a module'),
+      googleAMP: Selector('a').withText('Google AMP?'),
+      eCommerce: Selector('a').withText('E-commerce'),
+      travel: Selector('a').withText('Travel'),
+      productPage: Selector('a').withText('Product page'),
+      gallery: Selector('a').withText('Gallery'),
+      blog: Selector('a').withText('Blog'),
+      googleAMPcode: Selector('a').withText('https://github.com/mdyd-dev/amp-demo-pages'),
+      documentationWebsite: Selector('a').withText('https://documentation.platform-os.com'),
+      exampleCode: Selector('a').withText('https://github.com/mdyd-dev/marketplace-nearme-example')
     };
     this.button = {
       logout: Selector('.btn.btn-secondary.btn-sm')
