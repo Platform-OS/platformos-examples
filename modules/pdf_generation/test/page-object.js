35771ee3a72605490f18cdc8417990c9d052b0c4
Jacek <jacek@MacBook-Pro-Jacek.local>
2019-05-07 10:20:09 +0200

    e2e pdf_generation

---
+++ modules/pdf_generation/test/page-object.js
@@ -0,0 +1,11 @@
+import {Selector, ClientFunction, t} from 'testcafe';
+
+export default class PDFGeneration {
+  constructor () {
+    this.button = {
+      save: Selector ('.btn.btn-primary.btn-lg'),
+      PDF: Selector ('td a[href]'),
+      delete: Selector ('button.btn.btn-link'),
+    };
+  }
+}
