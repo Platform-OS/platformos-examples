66691fdc927be3ffe91b87fae33d9b2bf3a4ba9a
piotrze <piotr.bliszczyk@gmail.com>
2019-07-18 11:25:19 +0200

    Fix json in tests

--- modules/include/test/index.js
+++ modules/include/test/index.js
@@ -13,13 +13,13 @@ test('Simple include', async t => {
 test('Local variable using with', async t => {
   const actual = await getResultText({ name: 2, Selector });
 
-  await t.expect(actual).contains('{"maker":"Honda", "model":"CRX"}');
+  await t.expect(actual).contains('{"maker":"Honda","model":"CRX"}');
 });
 
 test('Iterating over collection using for', async t => {
   const actual = await getResultText({ name: 3, Selector });
   const expected =
-    '{"maker":"Honda", "model":"CRX"}{"maker":"Subaru", "model":"Forester"}{"maker":"Lexus", "model":"LFA"}';
+    '{"maker":"Honda","model":"CRX"}{"maker":"Subaru","model":"Forester"}{"maker":"Lexus","model":"LFA"}';
 
   await t.expect(actual).contains(expected);
 });
@@ -32,7 +32,7 @@ test('Private variables - Demonstration', async t => {
 
 test('Private variables - Exporting variable', async t => {
   const actual = await getResultText({ name: 5, Selector });
-  const expected = 'Car: {"honda":{"maker":"Honda", "model":"CRX", "year":"1991"}}';
+  const expected = 'Car: {"honda":{"maker":"Honda","model":"CRX","year":"1991"}}';
 
   await t.expect(actual).contains(expected);
 });
