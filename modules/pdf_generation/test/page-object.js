18c92b8f76ac2bf288899fa50cfa83d5d38879c0
Jacek <jacek@near-me.com>
2019-09-26 09:53:16 +0200

    e2e fix for save button selector

--- modules/pdf_generation/test/page-object.js
+++ modules/pdf_generation/test/page-object.js
@@ -3,7 +3,7 @@ import { Selector } from 'testcafe';
 export default class PDFGeneration {
   constructor() {
     this.button = {
-      save: Selector('.btn.btn-primary.btn-lg'),
+      save: Selector('button.btn.btn-primary.btn-lg'),
       PDF: Selector('td a[href]'),
       delete: Selector('button.btn.btn-link'),
     };
