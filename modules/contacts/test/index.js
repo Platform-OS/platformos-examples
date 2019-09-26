20a9673f649f6de0e42611fc7ae08966087212e2
piotrze <piotr.bliszczyk@gmail.com>
2019-09-26 09:36:12 +0200

    Contacts tests: check how many contacts are there before removing

--- modules/contacts/test/index.js
+++ modules/contacts/test/index.js
@@ -80,10 +80,9 @@ test('Update record works', async t => {
 test('Remove contact works', async t => {
   const emptyListMessage = 'There is no contacts yet. Use the form below to add some.';
 
+  await t.expect(await Selector('h3')).contains('Contacts count: 1');
   await t.click(contacts.button.deleteContact);
-  await t.expect(await getBtAlertText({
-    Selector
-  })).contains(contacts.alerts.removed);
+  await t.expect(await getBtAlertText({Selector})).contains(contacts.alerts.removed);
   await t.expect(await Selector('p').withText(emptyListMessage).exists).ok();
 });
 
