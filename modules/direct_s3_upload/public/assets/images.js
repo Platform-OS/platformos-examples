920e3cc1449875d5b0ca1f2e4d604f58562f8b8f
Paweł Kowalski <pavelloz@gmail.com>
2019-11-22 17:34:29 +0100

    Cleanup images examples

--- modules/direct_s3_upload/public/assets/images.js
+++ modules/direct_s3_upload/public/assets/images.js
@@ -12,22 +12,22 @@ const uppy = Uppy.Core({
   .use(Uppy.Dashboard, {
     inline: true,
     target: '#drag-drop-area',
-    replaceTargetContent: true,
-    showProgressDetails: true,
-    showLinkToFileUploadResult: false,
     note: 'Images only, up to 3 files, 2MB each',
     width: '100%',
-    height: 350,
-    proudlyDisplayPoweredByUppy: true
+    height: 350
   })
   .use(Uppy.DragDrop)
   .use(Uppy.GoldenRetriever)
   .use(Uppy.AwsS3, {
     getUploadParameters() {
+      // 1. Find form where presign data has been saved in data attribute
       const _url = _form.getAttribute('action');
+      // 2. Create Array from formdata object to make it easy to operate on the data
       const _formDataArray = Array.from(new FormData(_form));
+      // 3. Create a JSON object from array
       const _fields = _formDataArray.reduce((acc, cur) => ({ ...acc, [cur[0]]: cur[1] }), {});
 
+      // 4. Return resolved promise with Uppy - it will add file in file param
       return Promise.resolve({
         method: 'POST',
         url: _url,
@@ -36,18 +36,23 @@ const uppy = Uppy.Core({
     }
   });
 
-uppy.on('upload-success', (file, data) => {
+uppy.on('upload-success', (_file, data) => {
   const li = document.createElement('li');
   li.textContent = data.body.location;
-
   _log.appendChild(li);
 });
 
 uppy.on('complete', ({ failed, successful }) => {
-  Promise.all(successful.map(({ response }) => createImage(response.body.location))).then(() => {
+  /*
+    For every successfully uploaded image to s3, send request to the instance
+    that will create model with uploaded image url as direct_url param.
+  */
+  Promise.all(successful.map(({ response }) => createImage(response.body.location)))
+  .then(() => {
+    // Show confirmation box after all the images has been created
     Swal.fire({
       title: 'Images uploaded',
-      type: 'success',
+      icon: 'success',
       text: 'Press refresh to see the results',
       confirmButtonText: 'Refresh',
       showCloseButton: true
@@ -56,15 +61,18 @@ uppy.on('complete', ({ failed, successful }) => {
         return;
       }
 
+      // Reload page after "Refresh" button has been clicked inside Sweet Alert2
       window.location.reload();
     });
   });
 });
 
 const createImage = imageUrl => {
+  // Get logged in user id
   const userId = _form.dataset.s3UppyUserId;
 
-  return fetch('/update_profile/advanced/create_model', {
+  // Create model for this user with s3 image url
+  return fetch('/direct-s3-upload/images/create_model', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
